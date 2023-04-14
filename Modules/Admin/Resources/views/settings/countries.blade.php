3d99760ebc8e473f02330d2a159483b2443c6b2d
munthir100 <munthiromer100@gmail.com>
2023-04-14 20:43:01 +0200

    fix rating error

--- Modules/Admin/Resources/views/settings/countries.blade.php
+++ Modules/Admin/Resources/views/settings/countries.blade.php
@@ -1,5 +1,5 @@
 @extends('admin::Layouts.adminLayout')
-@section('title', __('Dashboard'))
+@section('title', __('Settings'))
 @section('content')
 <div class="app-content content ">
     <div class="content-overlay"></div>
