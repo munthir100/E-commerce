6beab2ba87efcc7b546888f7b0b189965187a13e
munthir100 <munthiromer100@gmail.com>
2023-05-01 10:13:33 +0200

    edit on         @include('admin::categories.subcategories', ['subcategories' => $subcategory->children])

--- Modules/Admin/Resources/views/Categories/subcategories.blade.php
+++ Modules/Admin/Resources/views/Categories/subcategories.blade.php
@@ -2,7 +2,7 @@
     @foreach($subcategories as $subcategory)
     <li data-jstree='{"icon" : "far fa-folder"}' id="row-{{$subcategory->id}}">{{ $subcategory->title }}
         @if(count($category->children) > 0)
-        @include('admin::Categories.subcategories', ['subcategories' => $subcategory->children])
+        @include('admin::categories.subcategories', ['subcategories' => $subcategory->children])
         @endif
     </li>
     @endforeach
