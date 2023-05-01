809288f3b94efe613a603846123b2690f5546265
munthir100 <munthiromer100@gmail.com>
2023-05-01 10:36:07 +0200

    test

--- Modules/Admin/Resources/views/Categories/subcategories.blade.php
+++ Modules/Admin/Resources/views/Categories/subcategories.blade.php
@@ -1,8 +1,9 @@
 <ul>
+    {{dd('d')}}
     @foreach($subcategories as $subcategory)
     <li data-jstree='{"icon" : "far fa-folder"}' id="row-{{$subcategory->id}}">{{ $subcategory->title }}
         @if(count($category->children) > 0)
-        @include('admin::categories.subcategories', ['subcategories' => $subcategory->children])
+        @include('admin::Categories.subcategories', ['subcategories' => $subcategory->children])
         @endif
     </li>
     @endforeach
